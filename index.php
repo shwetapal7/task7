d289b6d02c0c31e3a96c67445001d0df5a3ae5b5
shweta <94313665+shwetapal7@users.noreply.github.com>
2023-05-09 23:09:19 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="en">
+  <head>
+   
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@800&display=swap" rel="stylesheet">
+
+    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="navbar.css">
+
+    <title>Basic Banking System</title>
+  </head>
+
+  <body>
+  <?php
+  include 'navbar.php';
+  ?>
+  
+      <div class="container-fluid">
+      
+            <div class="row intro py-1">
+              <div class="col-sm-12 col-md">
+                <div class="heading text-center my-5">
+                  <h3>Welcome to</h3>
+                  <h1>SPARKS FOUNDATION BANK</h1>
+                </div>
+              </div>
+              <div class="col-sm-12 col-md img text-center">
+                <img class="im" src="bank1.jpg" class="img-fluid pt-2">
+              </div>
+            </div>
+
+      
+          
+          <br>
+          <h3 class="action" ><span style="font-family: 'Roboto Slab', serif">SELECT AN OPTION</h3></span>
+            <div class="column activity text-center">
+                  
+                  <div class="col-md act">
+                   
+                    <br>
+                    
+                    <br>
+                    <a href="transcationhistory.php"><button>Transfer History</button></a>
+                  </div>
+
+                  <div class="col-md act">
+                   
+                    <br>
+                    
+                    <br>
+                    <a href="transfermoney.php"><button>View Customers to transfer</button></a>
+                  </div>
+                  
+            </div>
+           
+      </div>
+      <br>
+     
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,18 @@
+<!-- navbar --> 
+
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+      <a class="navbar-brand" href="index.php"></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php">HOME</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="transcationhistory.php">HISTORY</a>
+              </li>
+          </div>
+       </nav>
